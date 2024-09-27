34555124ff1bf58c869748a918a2d725a0b16a6e
DSahalatyi <dsahalatyi@gmail.com>
2024-09-27 23:10:51 +0300

    Revert "Remove code duplication"
    This reverts commit cc095ebfe288a009aea2737352c5e3a42d5b4e2e.

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -14,8 +14,13 @@ class MovieSerializer(serializers.Serializer):
     actors = serializers.SerializerMethodField()
 
     def create(self, validated_data: dict) -> Movie:
+        genres_data = self.context["request"].data.get("genres", [])
+        actors_data = self.context["request"].data.get("actors", [])
         movie = Movie.objects.create(**validated_data)
-        self.set_genres_and_actors(movie)
+
+        movie.genres.set(genres_data)
+        movie.actors.set(actors_data)
+
         return movie
 
     def update(self, instance: Movie, validated_data: dict) -> Movie:
@@ -24,15 +29,13 @@ class MovieSerializer(serializers.Serializer):
         instance.duration = validated_data.get("duration", instance.duration)
         instance.save()
 
-        self.set_genres_and_actors(instance)
-        return instance
-
-    def set_genres_and_actors(self, movie: Movie) -> None:
         genres_data = self.context["request"].data.get("genres", [])
         actors_data = self.context["request"].data.get("actors", [])
 
-        movie.genres.set(genres_data)
-        movie.actors.set(actors_data)
+        instance.genres.set(genres_data)
+        instance.actors.set(actors_data)
+
+        return instance
 
     def get_genres(self, movie: Movie) -> list:
         return [genre.id for genre in movie.genres.all()]
@@ -41,39 +44,53 @@ class MovieSerializer(serializers.Serializer):
         return [actor.id for actor in movie.actors.all()]
 
 
-class BaseSerializer(serializers.Serializer):
-    def create(self, validated_data):
-        model_class = self.Meta.model
-        return model_class.objects.create(**validated_data)
-
-
-class ActorSerializer(BaseSerializer):
+class ActorSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     first_name = serializers.CharField(max_length=255)
     last_name = serializers.CharField(max_length=255)
 
-    class Meta:
-        model = Actor
+    def create(self, validated_data: dict) -> Actor:
+        return Actor.objects.create(**validated_data)
+
+    def update(self, instance: Movie, validated_data: dict) -> Actor:
+        instance.first_name = validated_data.get("first_name", instance.first_name)
+        instance.last_name = validated_data.get("last_name", instance.last_name)
+        instance.save()
+        return instance
 
 
-class GenreSerializer(BaseSerializer):
+class GenreSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
-
-    class Meta:
-        model = Genre
 
     def validate_name(self, name: str) -> str:
         if name and Genre.objects.filter(name=name).exists():
             raise serializers.ValidationError("Genre with this name already exists")
         return name
 
+    def create(self, validated_data: dict) -> Genre:
+        return Genre.objects.create(**validated_data)
 
-class CinemaHallSerializer(BaseSerializer):
+    def update(self, instance: Genre, validated_data: dict) -> Genre:
+        instance.name = validated_data.get("name", instance.name)
+        instance.save()
+        return instance
+
+
+class CinemaHallSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField(max_length=255)
     rows = serializers.IntegerField()
     seats_in_row = serializers.IntegerField()
 
-    class Meta:
-        model = CinemaHall
+    def create(self, validated_data: dict) -> CinemaHall:
+        return CinemaHall.objects.create(**validated_data)
+
+    def update(self, instance: CinemaHall, validated_data: dict) -> CinemaHall:
+        instance.name = validated_data.get("name", instance.name)
+        instance.rows = validated_data.get("rows", instance.rows)
+        instance.seats_in_row = validated_data.get(
+            "seats_in_row", instance.seats_in_row
+        )
+        instance.save()
+        return instance
